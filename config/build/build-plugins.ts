51d9df1d527f3249186ff2f6d34ff47cd5047d56
rise-the-flag <dev.denism@yandex.ru>
2024-03-04 13:33:54 +0600

    added HMR

--- config/build/build-plugins.ts
+++ config/build/build-plugins.ts
@@ -2,7 +2,6 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import * as webpack from "webpack";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import * as trace_events from "trace_events";
 
 
 export const buildPlugins = ({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] => [
@@ -16,5 +15,6 @@ export const buildPlugins = ({paths, isDev}: BuildOptions): webpack.WebpackPlugi
   }),
   new webpack.DefinePlugin({
     __IS_DEV__: JSON.stringify(isDev)
-  })
+  }),
+  new webpack.HotModuleReplacementPlugin(),
 ]
