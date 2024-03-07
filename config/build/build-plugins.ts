0e0ba93b64f91b866aa80c014ffe6f25a81864aa
rise-the-flag <dev.denism@yandex.ru>
2024-03-07 17:36:57 +0600

    fix dev plugins

--- config/build/build-plugins.ts
+++ config/build/build-plugins.ts
@@ -1,23 +1,30 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin'
-import * as webpack from 'webpack'
+import webpack, {type WebpackPluginInstance} from 'webpack'
 import { type BuildOptions } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
-export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => [
-  new HtmlWebpackPlugin({
-    template: paths.html
-  }),
-  new webpack.ProgressPlugin(),
-  new MiniCssExtractPlugin({
-    filename: 'css/[name].[contenthash:8].css',
-    chunkFilename: 'css/[name].[contenthash:8].css'
-  }),
-  new webpack.DefinePlugin({
-    __IS_DEV__: JSON.stringify(isDev)
-  }),
-  new webpack.HotModuleReplacementPlugin(),
-  new BundleAnalyzerPlugin({
-    openAnalyzer: false
-  })
-]
+
+export const buildPlugins = ({ paths, isDev }: BuildOptions): WebpackPluginInstance[] => {
+
+  const plugins: WebpackPluginInstance[] = [
+      new HtmlWebpackPlugin({
+        template: paths.html
+      }),
+      new webpack.ProgressPlugin(),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].css'
+      }),
+      new webpack.DefinePlugin({
+        __IS_DEV__: JSON.stringify(isDev)
+      }),
+  ]
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new BundleAnalyzerPlugin({openAnalyzer: false}))
+  }
+
+  return plugins;
+}
